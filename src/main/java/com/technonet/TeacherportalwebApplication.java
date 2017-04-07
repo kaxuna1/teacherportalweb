a59b8d264d4410a18451a491476a074315d49dbd
kaxuna1 <kaxgel11@gmail.com>
2017-04-07 18:45:31 +0400

    asa

--- src/main/java/com/technonet/TeacherportalwebApplication.java
+++ src/main/java/com/technonet/TeacherportalwebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class TeacherportalwebApplication {
-
+//asa
 	public static void main(String[] args) {
 		SpringApplication.run(TeacherportalwebApplication.class, args);
 	}
